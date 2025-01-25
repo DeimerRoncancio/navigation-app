1fe09731c83a6cd27e4b7e94b27fd546f34665a5
DeimerRoncancio <deimerroncancio@gmail.com>
2025-01-25 17:51:49 -0500

    Navegación implementada y color del navigation cambiado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
 import { Slot } from 'expo-router';
 import './global.css';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+
+import * as NavigationBar from 'expo-navigation-bar';
+
+const isAndroid = Platform.OS === 'android';
+if (isAndroid) NavigationBar.setBackgroundColorAsync('#60a5fa');
 
 export default function RootLayout() {
   return (
-      <Slot />
+    <View className='bg-blue-400 flex-1'>
+      <View className='mt-7 p-10'>
+        <Slot />
+      </View>
+      <StatusBar style="dark" />
+    </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,6 @@
+import { Link, Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App () {
-  return (
-    <View className="bg-blue-600 flex-1">
-      <Text className="text-white">Hola mundo</Text>
-    </View>
-  )
+  return <Redirect href='/home' />
 }
