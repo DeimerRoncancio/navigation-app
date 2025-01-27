3b703d386f0cf931badb473cfa2625e30ca54d06
DeimerRoncancio <deimerroncancio@gmail.com>
2025-01-26 19:52:15 -0500

    Cambio menor

--- app/(stack)/products/[id].tsx
+++ app/(stack)/products/[id].tsx
@@ -13,7 +13,7 @@ export default function ProductScreen() {
     <View>
       <Text className="text-2xl font-medium">{product.title}</Text>
       <Text className="text-lg font-normal mt-5">{product.description}</Text>
-      <Text className="text-violet-700 text-2xl mt-5">$ {product.price}</Text>
+      <Text className="text-violet-700 text-2xl mt-5 ">$ {product.price}</Text>
     </View>
   )
 }
